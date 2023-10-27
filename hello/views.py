ff942bacc1f341a586ffb95e482895d1acba601b
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-10-27 01:02:17 -0500

    new change

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("<h1>HOLA MUNDO</h1>")
+    return HttpResponse("<h1>pagina principal</h1>")
 
 def saludar(request):
-    return HttpResponse("<h2>nuevo saludo para mi cliente especial</h2>")
+    return HttpResponse("<h2>nuevo saludo para mi cliente</h2>")
